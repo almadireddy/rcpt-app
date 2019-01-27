1bdafffe8773fcf1dd7dacbee97e9c2eb8b0f04a
Al Madireddy <al.madireddy@gmail.com>
2019-01-27 05:26:44 -0600

    login using ur face

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import List from './List';
 import Stats from './Stats';
+import Login from './Login';
 
 const MainNavigator = createStackNavigator({
+  Login: { screen: Login },
   Home: { screen: List },
   Stats: { screen: Stats },
 });
